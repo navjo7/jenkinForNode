9966be0b9e53e036678085d962702598ccd439c8
navjo7 <navjotsinghdata@gmail.com>
2018-10-28 21:36:37 +1100

    assertion library expect included

--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 const utils = require('../utils/utils')
+const expect  = require('expect')
 it("should sum two numbers", () => {
     const result = utils.add(1, 2)
-    if(result!=3){
-        throw new Error(`incorrect result, expected: 3 but got ${result}`)
-    }
+    expect(result).toBe(3)
+    // if(result!=3){
+    //     throw new Error(`incorrect result, expected: 3 but got ${result}`)
+    // }
 })
